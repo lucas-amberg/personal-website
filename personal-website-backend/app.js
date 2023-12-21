5fcddd3d3423e116ad674cd2b6f02ea5856e7e66
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-12-20 19:24:12 -0800

    Initialize backend files

---
+++ personal-website-backend/app.js
@@ -0,0 +1,6 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
