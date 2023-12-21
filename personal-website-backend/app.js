de322be780fd258dc3c433e2e69d160288fe40da
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2023-12-20 20:17:17 -0800

    Configure DNS

--- personal-website-backend/app.js
+++ personal-website-backend/app.js
@@ -4,3 +4,13 @@ const app = express()
 const cors = require('cors')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+const path = require('path')
+
+app.use(cors())
+app.use(express.static('./dist'))
+
+app.get('*', function (req, res) {
+  res.sendFile(path.resolve(__dirname, './dist/index.html'));
+});
+
+module.exports = app
