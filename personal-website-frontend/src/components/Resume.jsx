6202be4397f64ce883965836a0f4a21ed8e10838
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-01-10 15:22:44 -0800

    Get resume button component to work properly

--- personal-website-frontend/src/components/Resume.jsx
+++ personal-website-frontend/src/components/Resume.jsx
@@ -1,9 +1,14 @@
 import { Fade } from "react-awesome-reveal"
+import resume from '../assets/Lucas_Amberg_Resume.pdf'
+import '../styles/resume.css'
 
 export default function Resume() {
   return(
     <div className="resume">
-
+      <Fade>
+        <h1>View my Resume:</h1>
+        <a href={resume} target="_blank">Resume</a>
+      </Fade>
     </div>
   )
 }
