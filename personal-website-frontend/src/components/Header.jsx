59570d67b261a1b7d59ef4cafebd5675d04155e5
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-01-10 22:49:01 -0800

    Create interval for scroll text:

--- personal-website-frontend/src/components/Header.jsx
+++ personal-website-frontend/src/components/Header.jsx
@@ -1,10 +1,23 @@
 import '../styles/header.css'
 import logo from '/logo.jpg'
 import { Fade } from 'react-awesome-reveal'
+import { useState, useEffect } from 'react';
 
 //This is the header component it is the first thing a user sees on
 //the page and is at the top of all other components
 function Header() {
+
+    const [showScroll, setShowScroll] = useState(false)
+
+    useEffect(() => {
+        //Implementing the setInterval method
+        const interval = setInterval(() => {
+            setShowScroll(!showScroll);
+        }, 5000);
+ 
+        //Clearing the interval
+        return () => clearInterval(interval);
+    }, [showScroll]);
 
 
     return(
@@ -17,6 +30,14 @@ function Header() {
                     </div>
                 </div>
             </Fade>
+            <div>
+                {showScroll ? 
+                <div className='scroll-down-text'>
+                    <Fade>
+                        <div>Scroll Down</div>
+                    </Fade>
+                </div> : ""}
+            </div>
         </div>
     )
 }
