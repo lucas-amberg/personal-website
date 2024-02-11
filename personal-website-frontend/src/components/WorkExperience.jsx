acb7557f109e82ecd013a24ce356d3d9a2e31974
Lucas Amberg <102396588+lucas-amberg@users.noreply.github.com>
2024-02-11 13:29:26 -0800

    Update work experience

--- personal-website-frontend/src/components/WorkExperience.jsx
+++ personal-website-frontend/src/components/WorkExperience.jsx
@@ -1,6 +1,7 @@
 import {Fade} from 'react-awesome-reveal'
 import '../styles/work-experience.css'
 import polaLogo from '../assets/images/logos/pola-logo.jpg'
+import hireBirdLogo from '../assets/images/logos/hirebird-logo.jpg'
 
 //This component is currently only used once because I only have one job experience
 //of note to put on my portfolio but it contains a section for job title,
@@ -29,6 +30,7 @@ function Job({direction, title, img, business, location, startDate, endDate, des
 
 //This is the descriptions for the job components
 const polaDescription = "Completed 100+ repairs for PC's, laptops, printers, and network devices, reducing downtime and ensuring smooth operations. Streamlined application and computer setup process through the implementation of customized Batch files and command line operations, resulting in a 30% reduction in overall setup time. Collaborated with co-workers to solve complex hardware/software issues on a daily basis."
+const hirebirdDescription = "Engineered a full stack website using Next.js and TypeScript, used by 14,000+ active users."
 
 //This is the workexperience component and it stores all of the jobs I have listed under my work experience
 function WorkExperience() {
@@ -36,6 +38,17 @@ function WorkExperience() {
         <Fade>
             <div className='work-experience-container'>
                 <h1>Work Experience</h1>
+
+                <Job title="Software Engineering Intern"
+                business="Hirebird"
+                img={hireBirdLogo}
+                alt="Hirebird Logo"
+                startDate="February 2024"
+                endDate="Present"
+                description={hirebirdDescription}
+                location="Albany, NY"
+                direction="right"
+                />
 
                 {/*Job #1 Port of LA*/}
                 <Job title='Information Technology Student Specialist'
